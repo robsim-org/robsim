0a9bdebcc4ba71f6abfa0c850ef1153fbe9d6ead
Caracol <rafaelfariasm@live.com>
2024-07-08 01:10:08 -0300

    fixed set motor speed

--- Robot/Robot.gd
+++ Robot/Robot.gd
@@ -10,19 +10,21 @@ func _ready():
 func _process(delta):
 	pass
 
+var motor0_speed_val:float = 0.0
+var motor1_speed_val:float = 0.0
+
 
 func set_speed_val(motor_idx:int, speed_val:float):
-	var motor = get_node("LeftHinge/LeftWheel") as RigidBody3D if (motor_idx==0) else get_node("RightHinge/RightWheel") as RigidBody3D
-	var torque = 5.0 * speed_val
-	var torque_vec = Vector3(torque, 0, 0)
-	var global_torque = global_transform.basis * torque_vec
-	motor.apply_torque(global_torque)
+	match motor_idx:
+		0:
+			motor0_speed_val = speed_val
+		1: 
+			motor1_speed_val = speed_val
 
-func _integrate_forces(state):
+func _manual_controll():
+	var torque = 3.0
 	var left_wheel = get_node("LeftHinge/LeftWheel") as RigidBody3D
 	var right_wheel = get_node("RightHinge/RightWheel") as RigidBody3D
-	const torque = 3.0
-	
 	if Input.is_action_pressed("left_up"):
 		var torque_vec = Vector3(torque, 0, 0)
 		var global_torque = global_transform.basis * torque_vec
@@ -39,3 +41,12 @@ func _integrate_forces(state):
 		var torque_vec = Vector3(-torque, 0, 0)
 		var global_torque = global_transform.basis * torque_vec
 		right_wheel.apply_torque(global_torque)
+func _integrate_forces(state):
+	var left_wheel = get_node("LeftHinge/LeftWheel") as RigidBody3D
+	var right_wheel = get_node("RightHinge/RightWheel") as RigidBody3D
+	var torque = 5.0
+	left_wheel.apply_torque(global_transform.basis * Vector3(torque * motor0_speed_val, 0, 0))
+	right_wheel.apply_torque(global_transform.basis * Vector3(torque * motor1_speed_val, 0, 0))
+	
+	_manual_controll()
+
